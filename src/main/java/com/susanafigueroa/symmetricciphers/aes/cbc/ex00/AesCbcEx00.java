f1c95e600632c34a80eee10943a7823762644791
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-22 17:16:41 +0100

    AES CBC Ex00 starting

---
+++ src/main/java/com/susanafigueroa/symmetricciphers/aes/cbc/ex00/AesCbcEx00.java
@@ -0,0 +1,15 @@
+package com.susanafigueroa.symmetricciphers.aes.cbc.ex00;
+
+/*
+Algoritmo AES (Advanced Encryption Standard)
+Modo CBC (Cipher Block Chaining)
+
+Encripta la frase: "Hola que tal" y desencríptala
+ */
+
+public class AesCbcEx00 {
+
+    private static final String ALGORITHM = "AES";
+
+
+}
---
+++ src/main/java/com/susanafigueroa/symmetricciphers/aes/readme.md
@@ -0,0 +1,9 @@
+# AES
+
+## MODOS DE OPERACIÓN SOPORTADOS POR EL ALGORITMO DE CIFRADO AES
+Para aplicaciones genéricas : CBC o GCM
+Para datos de flujo o tiempo real: CTR, CFB, OFB
+Para sistemas de archivos o discos: XTS
+Evitar ECB por la vulnerabilidad ante análisis de patrones
+
+![img.png](img.png)
