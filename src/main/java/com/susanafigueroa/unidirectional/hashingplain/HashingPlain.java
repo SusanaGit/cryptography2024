561686bcf50864f56b2dcdce7753ed69d4f4c333
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-26 14:22:59 +0100

    check if the file exists

--- src/main/java/com/susanafigueroa/unidirectional/hashingplain/HashingPlain.java
+++ src/main/java/com/susanafigueroa/unidirectional/hashingplain/HashingPlain.java
@@ -4,6 +4,7 @@ package com.susanafigueroa.unidirectional.hashingplain;
 Calculate the hash of the pom.xml file using the SHA-256 algorithm.
  */
 
+import java.io.File;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -14,7 +15,17 @@ public class HashingPlain {
         // we use MessageDigest because this class generates crypto hashes
         MessageDigest md = MessageDigest.getInstance("SHA-256");
 
+        String baseUrl = System.getProperty("user.dir") + File.separator;
 
+        String nameFile = "pom.xml";
+
+        File file = new File(baseUrl + nameFile);
+
+        if (file.exists()) {
+            System.out.println("The file exists.");
+        } else {
+            System.out.println("The file doesn't exist.");
+        }
     }
 
 }
