10cc6b6a8bf447cbceecd333c179384718d25557
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-26 21:33:21 +0100

    generate hash and print

---
+++ src/main/java/com/susanafigueroa/Util.java
@@ -0,0 +1,12 @@
+package com.susanafigueroa;
+
+public class Util {
+
+    public static String bytesToHex(byte[] hashBytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte hashByte : hashBytes) {
+            sb.append(String.format("%02X ", hashByte));
+        }
+        return sb.toString();
+    }
+}
--- src/main/java/com/susanafigueroa/unidirectional/hashingwithsalt/HashingSalt00.java
+++ src/main/java/com/susanafigueroa/unidirectional/hashingwithsalt/HashingSalt00.java
@@ -5,27 +5,30 @@ Generate a hash derived from the text "Hello, how are you? :)" using a secure pr
 (su@susanafigueroa.com) combined with the SHA-256 hash algorithm.
  */
 
+import com.susanafigueroa.Util;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class HashingSalt00 {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
         final String textToHash = "Hello, how are you? :)";
 
         // se combina con el texto a cifrar para hacerlo único
         final String salt = "su@susanafigueroa.com";
 
-        // configuro la keySpec
-        // convierte el texto a hashear a array char
-        // obtiene los bytes del salt
-        // cuantas veces se aplicará la función hash internamente -> la iteración con PBKDF2 no solo consiste
+        // configuro la keySpec -> esta especificación encapsula todos los parámetros necesarios para derivar la key
+        // la keySpec solo actúa como contenedor de parámetros
+        // param -> convierte el texto a hashear a array char
+        // param -> obtiene los bytes del salt
+        // param -> cuantas veces se aplicará la función hash internamente -> la iteración con PBKDF2 no solo consiste
         // en hacer el hash del hash, PBKDF2 añade la variable del resultado anterior y de lo que quieres hashear inicialmente
         // para obtener el nuevo hash, sino sería más sencillo de descifrar
-        // lenght de la key PBEKeySpec SHA-256 -> normalmente una de estas 128 / 256 / 512
+        // param -> lenght de la key PBEKeySpec SHA-256 -> normalmente una de estas 128 / 256 / 512
         PBEKeySpec keySpec = new PBEKeySpec(textToHash.toCharArray(), salt.getBytes(), 32, 512);
 
         // obtengo instancia SecretKeyFactory para poder generar la clave cripto
@@ -33,6 +36,10 @@ public class HashingSalt00 {
         // por el PBKDF2WithHmacSHA256)
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 
+        // obtengo el hash del textToHash unido a salt
+        byte[] hash = skf.generateSecret(keySpec).getEncoded();
+
+        System.out.println("hash of the textToHash with salt: " + Util.bytesToHex(hash));
 
     }
 
