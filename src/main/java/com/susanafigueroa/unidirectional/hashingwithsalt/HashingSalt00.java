68c07ad1529e068f409f36db72722bbdc0a976a1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-26 20:43:44 +0100

    hashing salt 00 - starting

---
+++ src/main/java/com/susanafigueroa/unidirectional/hashingwithsalt/HashingSalt00.java
@@ -0,0 +1,18 @@
+package com.susanafigueroa.unidirectional.hashingwithsalt;
+
+/*
+genera un hash derivado del texto "Hello, how are you? :)" utilizando un proceso seguro (PBKDF2) y
+una sal (su@susanafigueroa.com) combinados con el algoritmo de hash SHA-256.
+ */
+
+public class HashingSalt00 {
+
+    public static void main(String[] args) {
+
+        final String textToHash = "Hello, how are you? :)";
+
+
+
+    }
+
+}
